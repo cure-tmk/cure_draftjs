1fa0663069ba2cb18091a9e27e2ad36373c76e60
cure-tmk <cure.tomoki@gmail.com>
2021-03-13 16:23:54 +0900

    init ✨

---
+++ src/components/Editor.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const Editor: React.FC = () => {
+  return <p>Editor</p>
+}
+
+export default Editor
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import Editor from '~/components/Editor'
+
+ReactDOM.render(<Editor />, document.getElementById('react-root'))
